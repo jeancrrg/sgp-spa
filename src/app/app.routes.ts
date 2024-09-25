196a1d65db1400a12118b9e17c3ee495eec99d22
jeancrrg <jeancrg3232@gmail.com>
2024-09-24 22:57:40 -0300

    Inativado rotas que não serão utilizadas e alterada para rota padrão mostrar a tela de Sobre do sistema

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ export const AppRotas: Routes = [
     {
         path: '', component: AppLayoutComponent,
         children: [
-            { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(module => module.DashboardModule) },
+            { path: '', loadChildren: () => import('./view/sobre/sobre.module').then(module => module.SobreModule) },
             { path: 'produto', loadChildren: () => import('./view/produto/resumo-produto/resumo-produto.module')
                 .then(module => module.ResumoProdutoModule) },
             { path: 'produto/detalhe', loadChildren: () => import('./view/produto/detalhe-produto/detalhe-produto.module')
@@ -14,19 +14,21 @@ export const AppRotas: Routes = [
             { path: 'marca', loadChildren: () => import('./view/marca/marca.module').then(module => module.MarcaModule) },
             { path: 'departamento', loadChildren: () => import('./view/departamento/departamento.module').then(module => module.DepartamentoModule) },
             { path: 'categoria', loadChildren: () => import('./view/categoria/categoria.module').then(module => module.CategoriaModule) },
+            { path: 'sobre', loadChildren: () => import('./view/sobre/sobre.module').then(module => module.SobreModule) },
 
-
-
+            /*
             { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
             { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
             { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
             { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
             { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) },
-            { path: 'sobre', loadChildren: () => import('./view/sobre/sobre.module').then(module => module.SobreModule) }
+             */
         ]
     },
+    /*
     { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
     { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
     { path: 'notfound', component: NotfoundComponent },
+     */
     { path: '**', redirectTo: '/notfound' }
 ];
